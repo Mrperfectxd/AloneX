030c1c5f3a7c8f9c7bd58debc7966ceb33507b97
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2026-06-01 12:10:56 +0530

    𝐂ᴏᴍᴍᴀɴᴅ 𝐅ɪx 𝐁ʏ 𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ

--- AloneX/helpers/_inline.py
+++ AloneX/helpers/_inline.py
@@ -198,7 +198,7 @@ class Inline:
                     self.ikb(text=lang["aloneowner"], user_id=config.OWNER_ID, style=ButtonStyle.DANGER),
                     self.ikb(
                         text=lang["source"],
-                        url="https://github.com/TeamAloneOp/AloneX", style=ButtonStyle.DANGER
+                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x54\x65\x61\x6D\x41\x6C\x6F\x6E\x65\x4F\x70\x2F\x41\x6C\x6F\x6E\x65\x58", style=ButtonStyle.DANGER
                     )
                 ]
             ]
